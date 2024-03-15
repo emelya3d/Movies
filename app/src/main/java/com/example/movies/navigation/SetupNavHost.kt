32cd988e44f6896a43cc297e7ed4693e58581133
emelya3d <emelya3d @gmail.com>
2024-03-15 15:32:07 +0300

    add Screens

--- app/src/main/java/com/example/movies/navigation/SetupNavHost.kt
+++ app/src/main/java/com/example/movies/navigation/SetupNavHost.kt
@@ -1,2 +1,34 @@
 package com.example.movies.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Constraints
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.movies.ui.theme.utils.Constants
+
+sealed class Screens(val route: String) {
+    object Splash : Screens(route = Constants.Screens.SPLASH_SCREEN)
+    object Main : Screens(route = Constants.Screens.MAIN_SCREEN)
+    object Details : Screens(route = Constants.Screens.DETAILS_SCREEN)
+}
+
+@Composable
+fun SetupNavHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screens.Splash.route
+    ) {
+        composable(route = Screens.Splash.route) {
+
+        }
+        composable(route = Screens.Main.route) {
+
+        }
+        composable(route = Screens.Details.route) {
+
+        }
+    }
+
+}
