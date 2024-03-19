33ae54529afe3370178c3df0480b5132d67beffd
emelya3d <emelya3d @gmail.com>
2024-03-19 15:43:49 +0300

    main screen

--- app/src/main/java/com/example/movies/screens/MainScreen.kt
+++ app/src/main/java/com/example/movies/screens/MainScreen.kt
@@ -1,8 +1,45 @@
 package com.example.movies.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 @Composable
-fun MainScreen(){
+fun MainScreen() {
 
+    ElevatedCard()
+}
+
+@Composable
+fun ElevatedCard() {
+
+        ElevatedCard(Modifier.size(width = 180.dp, height = 100.dp)
+
+              ) {
+            Box(Modifier.fillMaxSize()) {
+                Text("Card content", Modifier.align(Alignment.Center))
+    }
+
+        }
+    }
+
+
+
+@Preview
+@Composable
+fun ElevatedCardSample() {
+    ElevatedCard(Modifier.size(width = 180.dp, height = 100.dp)) {
+        Box(Modifier.fillMaxSize()) {
+            Text("Card content", Modifier.align(Alignment.Center))
+        }
+    }
 }
