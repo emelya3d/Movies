2f86599c1f1d592c496aa1bf70dbb122ee43e780
emelya3d <emelya3d @gmail.com>
2024-03-26 08:35:00 +0300

    //Compose MainScreen.kt add column, row

--- app/src/main/java/com/example/movies/screens/MainScreen.kt
+++ app/src/main/java/com/example/movies/screens/MainScreen.kt
@@ -1,8 +1,11 @@
 package com.example.movies.screens
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,9 +15,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardDefaults.cardElevation
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,9 +29,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.example.movies.MainViewModel
 import com.example.movies.data.models.Movies
 
@@ -33,9 +44,10 @@ import com.example.movies.data.models.Movies
 fun MainScreen(navController: NavController, viewModel: MainViewModel) {
     val allMovies = viewModel.allMovies.observeAsState(listOf()).value
 
-    Surface(
+    Surface(//color= MaterialTheme.colorScheme.error,
         modifier = Modifier
             .fillMaxSize()
+
     ) {
         LazyColumn(
             modifier = Modifier,
@@ -52,11 +64,53 @@ fun MainScreen(navController: NavController, viewModel: MainViewModel) {
 @Composable
 fun MovieItem(item: Movies) {
     Card(
+        // colors = CardDefaults.cardColors(Color.Unspecified),
         elevation = cardElevation(4.dp),    //elevation =4.dp не катит
         modifier = Modifier
-            .padding(top = 8.dp),
+            .padding(top = 8.dp)
+            .clickable { }
 
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp)
         ) {
+            Image(
+                painter = rememberAsyncImagePainter(item.image.medium),
+                contentDescription = null,
+                modifier = Modifier.size(128.dp)
+            )
+            Column {
+                Text(
+                    text = item.name,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.W400
+                )
+                Row {
+                    Text(
+                        text = "Rating :",
+                        fontWeight = FontWeight.W400
+                    )
+                    Text(text = item.rating.average.toString())
+                }
+                Row {
+                    Text(
+                        text = "Genre :",
+                        fontWeight = FontWeight.W400
+                    )
+                    item.genres.take(2).forEach{ Text(text = "$it")} // берем 2 иначе жанры полезут на другую строку
+                }
+                Row {
+                    Text(
+                        text = "Premiered :",
+                        fontWeight = FontWeight.W400
+                    )
+                    Text(text = item.premiered)
+                }
+
+            }
+        }
 
     }
 
